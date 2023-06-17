4e57505186bb5e31a915bac0fa759c672a199678
Dylan Brain <dylanbrain@googlemail.com>
2023-06-17 20:26:33 +0100

    spelling

--- src/pages/about.js
+++ src/pages/about.js
@@ -47,14 +47,14 @@ const About = () => {
           <div className="about-columns flex w-full flex-col items-center py-12 text-white sm:flex-row sm:items-start sm:justify-between">
             <div className="left-col w-11/12 sm:w-[45%]">
               <p>
-                Tailroad is a web design and development agency with roots in
+                tailroad is a web design and development agency with roots in
                 translation services. Our journey began in Japan, where we honed
                 our skills in language and cultural translation. As we grew and
                 expanded, we discovered a passion for web design and development
                 and decided to pursue it as our main focus.
               </p>
               <p>
-                The name Tailroad is a direct translation of Onomichi, the
+                The name tailroad is a direct translation of Onomichi, the
                 picturesque town in Japan where our company was founded.
                 Onomichi is known for its winding streets and charming
                 atmosphere, and we take inspiration from this in our approach to
@@ -62,7 +62,7 @@ const About = () => {
               </p>
               <p>
                 The town is also famous for its suspension bridges and abundance
-                of cat which are represented in our logo as a nod to our roots.
+                of cats which are represented in our logo as a nod to our roots.
               </p>
             </div>
             <div className="right-col w-11/12 sm:w-[45%]">
@@ -79,7 +79,7 @@ const About = () => {
                 the value of diverse perspectives. These values are at the core
                 of our approach to design and development, and we strive to
                 create solutions that are tailored to our clients' specific
-                needs and goals. At Tailroad, we are dedicated to helping
+                needs and goals. At tailroad, we are dedicated to helping
                 businesses succeed in the digital age. Whether you need a new
                 website or a custom app, we have the skills and expertise to
                 bring your vision to life.
